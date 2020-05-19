47d69f1cfce0c3b9d4b7238bf8441b7cc53ca770
Erick <erickhenrique321@gmail.com>
2020-05-19 19:02:39 -0300

    Todo add task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Todo = () => <h1>Todo</h1>;
+const Todo = () => {
+  const [task, updateTask] = useState("");
+  const [tasks, updateTasks] = useState([]);
+
+  const handleInputChange = (event) => updateTask(event.target.value);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (task.trim()) {
+      updateTasks([...tasks, task]);
+      updateTask("");
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          onChange={handleInputChange}
+          placeholder="type a new task here"
+          type="text"
+          value={task}
+        />
+        <button type="submit">Add new task</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Task</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((t, index) => (
+            <tr key={index}>
+              <td>{t}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default Todo;
